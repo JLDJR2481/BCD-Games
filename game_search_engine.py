5e38b893600d630b84d53d2fc68cc37c90b685a0
Jose/Chelu/Xulo <jjesusramon@cifpfbmoll.eu>
2024-04-22 16:45:18 +0200

    feat(): Versión preliminar de SE

--- game_search_engine.py
+++ game_search_engine.py
@@ -1,30 +1,20 @@
 from redis import Redis
-from pymongo import MongoClient
+# from pymongo import MongoClient
 import dotenv
+import time
+import httpx
 
 # Carga de variables del sistema
 dotenv.load_dotenv(dotenv.find_dotenv())
-
-# Conexión a MongoDB en Remoto
-uri = dotenv.get_key(dotenv.find_dotenv(), 'MONGO_URI')
-
-try:
-    client = MongoClient(uri)
-    db = client.get_database('Games')
-    collection = db.get_collection('games-id')
-    print("Conexión a la base de datos establecida.")
-
-except Exception as e:
-    print(e)
-
+env = dotenv.dotenv_values()
 
 # Conexión a Redis en Remoto
 try:
     redis = Redis(
-        host=dotenv.get_key(dotenv.find_dotenv(), 'REDIS_HOST'),
+        host=env['REDIS_HOST'],
         port=13121,
-        username=dotenv.get_key(dotenv.find_dotenv(), 'REDIS_USER'),
-        password=dotenv.get_key(dotenv.find_dotenv(), 'REDIS_PASSWORD')
+        username=env['REDIS_USER'],
+        password=env['REDIS_PASSWORD'],
     )
     print("Conexión a Redis establecida.")
 except Exception as e:
@@ -34,39 +24,61 @@ except Exception as e:
 
 
 def get_game(input):
-    input = input.lower()
-    games = redis_search(input)
+    games = redis_search(input.capitalize())
 
     if games:
-        print('Existe en Redis')
         return (game.decode("utf-8") for game in games)
 
     else:
-        print('No existe en Redis')
         games = mongo_search(input)
         if games:
             store_cache_redis(games)
-
         return games
 
 
-# Función que busca los juegos en MongoDB
 def mongo_search(input):
-    query = {"name": {"$regex": f"^{input}", "$options": "i"}}
-    mongo_games = collection.find(query)
+    urlEndpoint = env["URL_ENDPOINT"]
+    url = f"{urlEndpoint}/action/find"
+    data = {
+        "dataSource": env['DATASOURCE'],
+        "database": env['DATABASE'],
+        "collection": env['COLLECTION'],
+        "filter": {"name": {"$regex": f"^{input}", "$options": "i"}},
+        "projection": {
+            "_id": 0
+        },
+        # "limit": 10
+    }
 
-    return list(map(lambda game: game.get('name'), mongo_games))
+    headers = {
+        "Content-Type": "application/json",
+        "api-key": env['MONGO_API_KEY']
+
+    }
+
+    response = httpx.post(url, json=data, headers=headers)
+
+    mongo_games = response.json()
+
+    return list(map(lambda game: game.get('name'), mongo_games.get('documents')))
 
 # Función que busca los juegos en Redis
 
 
 def redis_search(input):
-    all_games = {game.decode("utf-8") for game in redis.smembers('games')}
-    input_set = {game for game in all_games if game.startswith(input)}
-    return all_games & input_set
+    games = redis.zrangebylex('games', f"[{input}", f"[{input}\xff")
+    return games
 
 # Procedimiento para almacenar los juegos dentro de la caché de Redis
 
 
 def store_cache_redis(games):
-    redis.sadd('games', *games)
+    redis.zadd('games', {game: 0 for game in games})
+
+
+if __name__ == "__main__":
+
+    start_time = time.time()
+    get_game('Cou')
+    end_time = time.time()
+    print(f"Tiempo total de ejecución: {end_time - start_time} segundos.")
