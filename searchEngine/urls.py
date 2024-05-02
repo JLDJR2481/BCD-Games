d077c7b51bc6293d07cbfe0d11afbbfd51170171
Jose/Chelu/Xulo <jjesusramon@cifpfbmoll.eu>
2024-05-02 23:59:17 +0200

    feat(): Añadido capacidad de poder usar avatar

--- searchEngine/urls.py
+++ searchEngine/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from .views import SearchEngineView, ResultsListView, ResultDetailView
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path("", SearchEngineView.as_view(), name="searchEngine"),
     path("results/", ResultsListView.as_view(), name="results"),
     path("results/<str:game_id>", ResultDetailView.as_view(), name="details"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
