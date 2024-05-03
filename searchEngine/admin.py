e7f2523cbf6693822bfe636fef6827cf7dba96df
Jose/Chelu/Xulo <jjesusramon@cifpfbmoll.eu>
2024-05-03 23:52:55 +0200

    feat(): Implementado admin en CustomUser

--- searchEngine/admin.py
+++ searchEngine/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import CustomUser
 
-# Register your models here.
+
+class CustomModelUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'profile_avatar')
+
+
+admin.site.register(CustomUser, CustomModelUserAdmin)
