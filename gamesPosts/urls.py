6347f344d268d3c353b468218fc818b5c823a4e2
Jose/Chelu/Xulo <jjesusramon@cifpfbmoll.eu>
2024-05-05 13:04:27 +0200

    feat(): Mejora de template Post. Contadores de me gustas y de comentarios. Mini lógica de me gustas y no me gustas

--- gamesPosts/urls.py
+++ gamesPosts/urls.py
@@ -5,4 +5,5 @@ from .views import *
 urlpatterns = [
     path("", HomeBlogView.as_view(), name="gamesPosts"),
     path('<int:post_id>/', PostDetailView.as_view(), name='post-details'),
+    path('<int:post_id>/like/', PostLike.as_view(), name='post-like'),
 ]
--- gamesPosts/views.py
+++ gamesPosts/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post, Comment, Like
 from searchEngine.models import CustomUser
+from django.views import View
 
 
 class HomeBlogView(ListView):
@@ -19,9 +20,12 @@ class PostDetailView(DetailView):
         post = Post.objects.get(id=post_id)
 
         author = CustomUser.objects.get(id=post.author_id)
-        comments = Comment.objects.filter(post=post).values()
+        comments = Comment.objects.filter(
+            post=post).order_by('-comment_date').values()
+        likes = Like.objects.filter(post=post).values()
 
         comments_data = []
+        likes_data = []
 
         for comment in comments:
 
@@ -33,4 +37,40 @@ class PostDetailView(DetailView):
             }
             comments_data.append(comment_data)
 
-        return render(request, self.template_name, {'post': post, "author": author, "comments": comments_data})
+        for like in likes:
+            user = CustomUser.objects.get(id=like["user_id"])
+            like_data = {
+                "user": user
+            }
+            likes_data.append(like_data)
+
+        user_has_liked = post.user_has_liked(request.user)
+
+        likes_count = post.count_likes()
+        comments_count = post.count_comments()
+
+        return render(request, self.template_name, {
+            'post': post,
+            "author": author,
+            "comments": comments_data,
+            "likes": likes_data,
+            "user_has_liked": user_has_liked,
+            "likes_count": likes_count,
+            "comments_count": comments_count,
+
+        })
+
+
+class PostLike(LoginRequiredMixin, View):
+    def post(self, request, post_id):
+        post = Post.objects.get(id=post_id)
+        user = request.user
+
+        if post.user_has_liked(user):
+            like = Like.objects.get(user=user, post=post)
+            like.delete()
+        else:
+            like = Like(user=user, post=post)
+            like.save()
+
+        return redirect('post-details', post_id=post_id)
