587bcf9fa16dc2de363dad2b421da9d7c9cb13a1
Jose/Chelu/Xulo <jjesusramon@cifpfbmoll.eu>
2024-05-08 16:19:23 +0200

    feat(): Añadido modelo para admin

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import GameScores
+
+
+class ScoreAdmin(admin.ModelAdmin):
+    list_display = ('user', 'score', 'date')
+
+
+admin.site.register(GameScores, ScoreAdmin)
