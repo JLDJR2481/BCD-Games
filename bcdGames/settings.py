705bd8bc7581123d9e9a8c354568b435f8714996
Jose/Chelu/Xulo <jjesusramon@cifpfbmoll.eu>
2024-05-10 16:21:27 +0200

    fix(): Arreglo de nombre

--- bcdGames/settings.py
+++ bcdGames/settings.py
@@ -147,5 +147,5 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get("SMTP_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("STMP_PASS")
+EMAIL_HOST_PASSWORD = os.environ.get("SMTP_PASS")
 DEFAULT_FROM_EMAIL = "bcd.games2001@gmail.com"
