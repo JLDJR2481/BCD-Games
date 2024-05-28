63b82f77ad81902718e81a564221f4c704250d05
Jose/Chelu/Xulo <jjesusramon@cifpfbmoll.eu>
2024-05-28 16:35:09 +0200

    fix(): Borrado BBDD por datos sensibles. Cambio de enfoque

--- migration_mongo_sql.py
+++ migration_mongo_sql.py
@@ -1,31 +1,27 @@
-import dotenv
-from pymongo import MongoClient
 from searchEngine.models import Game
+import json
 
-# Script para lanzar en la terminal de Django (py manage.py shell) que permite interactuar en los juegos guardados en un cliente MongoDB en Docker y pasarlo al ORM de Django (en este caso, en SQLite3)
+# Script para la terminal de Django que permite, desde un archivo JSON, añadir todos los videojuegos en la BBDD SQLite3
 
-dotenv.load_dotenv()
 
-db_client = MongoClient('localhost', 27017)
-bcdGamesDB = db_client['bcdGames']
-gamesCollection = bcdGamesDB['games']
+with open('games.json', 'r', encoding="utf-8") as file:
+    files = json.load(file)
 
-games = gamesCollection.find()
-
-for game in games:
+for file in files:
+    data = file["fields"]
     Game.objects.create(
-        game_id=game['id'],
-        name=game['name'],
-        translated_description_es=game['translated_description_es'],
-        metacritic=game['metacritic'],
-        released=game['released'],
-        background_image=game['background_image'],
-        average_rating=game['average_rating'],
-        platforms=game['platforms'],
-        stores=game['stores'],
-        genres=game['genres'],
-        tags=game['tags'],
-        developers=game['developers'],
-        publishers=game['publishers'],
-        esbr_ratings=game['esbr_ratings']
+        game_id=data['game_id'],
+        name=data['name'],
+        translated_description_es=data['translated_description_es'],
+        metacritic=data['metacritic'],
+        released=data['released'],
+        background_image=data['background_image'],
+        average_rating=data['average_rating'],
+        platforms=data['platforms'],
+        stores=data['stores'],
+        genres=data['genres'],
+        tags=data['tags'],
+        developers=data['developers'],
+        publishers=data['publishers'],
+        esbr_ratings=data['esbr_ratings']
     )
